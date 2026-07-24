fa5a85421b0420e667457490502a041f145ff0c3
AnandChaudhary590 <anandchaudhary76900@gmail.com>
2026-07-25 01:22:06 +0530

    Setup Express backend server

---
+++ backend/src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Insurance Management Platform API is running",
+  });
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
